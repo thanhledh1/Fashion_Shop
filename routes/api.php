ce047e30d5f800ca4cde0e6b41616fde38217b1a
thanhledh1 <thanhle.vt2@gmail.com>
2024-03-07 09:32:44 +0700

    logout

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,8 @@ Route::post('/checklogin', [CustomerController::class, 'checklogin']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
 
 
 
